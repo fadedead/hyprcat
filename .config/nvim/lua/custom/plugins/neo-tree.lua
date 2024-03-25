293c55ec7c7a3bac7ac94abca88c8df484a07b6a
fadedead <fadedeadd@gmail.com>
2024-03-25 17:59:53 +0530

    nvim show hidden files

--- .config/nvim/lua/custom/plugins/neo-tree.lua
+++ .config/nvim/lua/custom/plugins/neo-tree.lua
@@ -23,14 +23,12 @@ return {
                     end
                 },
             },
-            opts = {
-                filesystem = {
-                    filtered_items = {
-                        visible = true,
-                        show_hidden_count = true,
-                        hide_dotfiles = false,
-                        hide_gitignored = true
-                    }
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    show_hidden_count = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = true
                 }
             }
         }
